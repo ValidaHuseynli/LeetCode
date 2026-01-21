b290db778ac382cf3b4544cc5d30c1f6bbed3f37
ValidaH <validah@azerconnect.az>
2026-01-21 15:16:12 +0400

    4

---
+++ LargestOddNumberinString.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class LargestOddNumberinString {
+    public String largestOddNumber(String num) {
+
+        for(int i=num.length()-1; i>=0; i--){
+            int a = num.charAt(i) - '0';
+            if(a%2==1)
+                return num.substring(0, i+1);
+
+        }
+        return "";
+    }
+}
